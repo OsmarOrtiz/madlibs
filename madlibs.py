18f9e59376602126298a029f0a1214403de22a7e
OsmarOrtiz <147085021+OsmarOrtiz@users.noreply.github.com>
2023-10-11 12:55:57 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+adj = input("Adjetive: ")
+adj2 = input("Adjetive: ")
+verb = input("verb: ")
+verb2 = input("verb: ")
+
+madlibs = f"I love programing cuz it makes me feel {adj} and {adj2} at the same time. So remember to {verb} and {verb2} every time your are programing"
+print (madlibs)
